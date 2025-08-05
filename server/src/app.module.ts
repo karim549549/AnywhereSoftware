c113657f2ca3aa332f83f2e03c87efd6dc88cb4e
karim khaled <karimkhaled549@gmail.com>
2025-08-05 20:40:20 +0300

    added  client

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizModule } from './quiz/quiz.module';
+import { AnnouncementModule } from './announcement/announcement.module';
+import { AuthModule } from './auth/auth.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [QuizModule, AnnouncementModule, AuthModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
